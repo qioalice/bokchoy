531c419a1c3d802f8a0bb0df4d7fae8b1752110f
Ilya Stroy <qioalice@gmail.com>
2020-10-02 00:57:23 +0300

    Finished refactor of bokchoy.go, bokchoy_private.go

--- bokchoy.go
+++ bokchoy.go
@@ -1,133 +1,185 @@
+//
+// ORIGINAL PACKAGE
+// ( https://github.com/thoas/bokchoy )
+//
+//     Copyright © 2019. All rights reserved.
+//     Author: Florent Messa
+//     Contacts: florent.messa@gmail.com, https://github.com/thoas
+//     License: https://opensource.org/licenses/MIT
+//
+// HAS BEEN FORKED, HIGHLY MODIFIED AND NOW IS AVAILABLE AS
+// ( https://github.com/qioalice/bokchoy )
+//
+//     Copyright © 2020. All rights reserved.
+//     Author: Ilya Stroy.
+//     Contacts: qioalice@gmail.com, https://github.com/qioalice
+//     License: https://opensource.org/licenses/MIT
+//
+
 package bokchoy
 
 import (
 	"context"
-	"fmt"
-	"log"
-	"os"
-	"os/user"
 	"strings"
 	"sync"
 
-	"github.com/thoas/bokchoy/logging"
-	"github.com/thoas/go-funk"
-
-	"github.com/pkg/errors"
+	"github.com/qioalice/ekago/v2/ekadanger"
+	"github.com/qioalice/ekago/v2/ekaerr"
+	"github.com/qioalice/ekago/v2/ekalog"
 )
 
-// Bokchoy is the main object which stores all configuration, queues
-// and broker.
-type Bokchoy struct {
-	cfg            Config
-	wg             *sync.WaitGroup
-	defaultOptions *Options
-	broker         Broker
-	queues         map[string]*Queue
-	middlewares    []func(Handler) Handler
-	servers        []Server
-
-	Serializer Serializer
-	Logger     logging.Logger
-	Tracer     Tracer
-}
+type (
+	// Bokchoy is the main object which stores all configuration,
+	// queues and broker.
+	Bokchoy struct {
+		sema           *sync.Mutex
+		wg             *sync.WaitGroup
+		defaultOptions *Options
+		broker         Broker
+		queues         map[string]*Queue
+		middlewares    []func(Handler) Handler
+		serializer     Serializer
+		logger         *ekalog.Logger
+		isStarted      bool
+	}
+)
 
 // New initializes a new Bokchoy instance.
-func New(ctx context.Context, cfg Config, options ...Option) (*Bokchoy, error) {
-	opts := newOptions()
-	for i := range options {
-		options[i](opts)
+func New(ctx context.Context, options ...Option) (*Bokchoy, *ekaerr.Error) {
+
+	optionsObject := defaultOptions()
+	for i, n := 0, len(options); i < n; i++ {
+		if options[i] != nil {
+			options[i](optionsObject)
+		}
 	}
 
-	var (
-		err    error
-		tracer Tracer
-	)
+	// Validate options.
+	// Some options are must presented by user.
+	switch {
 
-	logger := logging.NewNopLogger()
-	if opts.Logger != nil {
-		logger = opts.Logger
+	case optionsObject.Broker == nil:
+		// Some kind of serializers may work OK even if its receiver is nil.
+		// It's bad design, but OK.
+		return nil, ekaerr.InitializationFailed.
+			New("Bokchoy: Broker must be presented. " +
+				"Use WithBroker() option as a part of constructor argument").
+			Throw()
+
+	case optionsObject.Serializer == nil:
+		// Some kind of serializers may work OK even if its receiver is nil.
+		// It's bad design, but OK.
+		return nil, ekaerr.InitializationFailed.
+			New("Bokchoy: Serializer must be presented. " +
+				"Use WithSerializer() option as a part of constructor argument").
+			Throw()
 	}
 
-	tracer = opts.Tracer
-	if tracer == nil {
-		tracer = NewLoggerTracer(logger)
+	// Validate options part 2.
+	// Now it's only warning messages.
+
+	logger := ekalog.WithThis() // an easy way to get default logger
+	// User can "disable" logging passing nil or invalid logger.
+	// Thus there is no either nil check nor logger.IsValid() call.
+	if optionsObject.loggerIsPresented {
+		logger = optionsObject.Logger
 	}
 
-	wg := &sync.WaitGroup{}
-	bok := &Bokchoy{
-		cfg:            cfg,
-		Serializer:     newSerializer(cfg.Serializer),
-		queues:         make(map[string]*Queue),
-		wg:             wg,
-		Logger:         logger,
-		Tracer:         tracer,
-		defaultOptions: opts,
-		servers:        opts.Servers,
+	if ekadanger.TakeRealAddr(optionsObject.Broker) == nil && logger.IsValid() {
+		logger.Warn("Bokchoy.Initialization: " +
+			"You present a Broker with nil underlying address value. It's ok?")
 	}
 
-	if opts.Serializer != nil {
-		bok.Serializer = opts.Serializer
+	if ekadanger.TakeRealAddr(optionsObject.Serializer) == nil && logger.IsValid() {
+		logger.Warn("Bokchoy.Initialization: " +
+			"You present a Serializer with nil underlying address value. It's ok?")
 	}
 
-	if opts.Broker != nil {
-		bok.broker = opts.Broker
-	} else {
-		bok.broker = newBroker(cfg.Broker,
-			logger.With(logging.String("component", "broker")))
+	if !optionsObject.Initialize && logger.IsValid() {
+		logger.Warn("Bokchoy.Initialization: " +
+			"You did not present initialize option (WithInitialize(true)). " +
+			"Do you already initialize your broker manually?")
 	}
 
-	if opts.Initialize {
-		bok.Logger.Debug(ctx, fmt.Sprintf("Connecting to %s...", bok.broker))
+	// Options has been validated.
+	// It's OK and safe to proceed.
 
-		err = bok.broker.Initialize(ctx)
-		if err != nil {
-			return nil, errors.Wrap(err, "unable to initialize broker")
+	if optionsObject.Initialize {
+
+		if logger.IsValid() {
+			logger.Debug("Bokchoy.Initialization: " +
+				"Initialize presented Broker...",
+				"bokchoy_init_broker", optionsObject.Broker.String())
 		}
 
-		bok.Logger.Debug(ctx, fmt.Sprintf("Connected to %s", bok.broker))
+		if err := optionsObject.Broker.Initialize(ctx); err.IsNotNil() {
+			return nil, err.
+				AddMessage("Bokchoy: Failed to initialize presented Broker").
+				AddFields("bokchoy_init_broker", optionsObject.Broker.String())
+		}
+
+		if logger.IsValid() {
+			logger.Debug("Bokchoy.Initialization: " +
+				"Initialized successfully. Ready to use.",
+				"bokchoy_init_broker", optionsObject.Broker.String())
+		}
 	}
 
-	for i := range cfg.Queues {
-		bok.Queue(cfg.Queues[i].Name)
+	bok := &Bokchoy{
+		broker:         optionsObject.Broker,
+		serializer:     optionsObject.Serializer,
+		queues:         make(map[string]*Queue),
+		wg:             &sync.WaitGroup{},
+		sema:           &sync.Mutex{},
+		logger:         logger,
+		defaultOptions: optionsObject,
+	}
+
+	for i, n := 0, len(optionsObject.Queues); i < n; i++ {
+		if optionsObject.Queues[i] != "" {
+			_ = bok.Queue(optionsObject.Queues[i])
+		}
+	}
+
+	if !optionsObject.DisableOutput {
+		bok.displayOutput(bok.queueNames())
 	}
 
 	return bok, nil
 }
 
-// Use append a new middleware to the system.
-func (b *Bokchoy) Use(sub ...func(Handler) Handler) *Bokchoy {
-	b.middlewares = append(b.middlewares, sub...)
+// Queue gets or creates a new queue.
+//
+// If Run() has been called already, the new queue's consumers will be start
+// immediately (if it's a new queue, and if Bokchan has not been stopped yet).
+//
+// If queue with the given 'name' has already declared,
+// the Queue method just returns it, but if at least one Option is provided
+// (even nil), the queue will be recreated with provided options.
+func (b *Bokchoy) Queue(name string, options ...Option) *Queue {
 
-	return b
-}
+	b.sema.Lock()
+	defer b.sema.Unlock()
 
-// Empty empties initialized queues.
-func (b *Bokchoy) Empty(ctx context.Context) error {
-	for i := range b.queues {
-		err := b.queues[i].Empty(ctx)
-		if err != nil {
-			return err
+	if b.isStarted {
+		// TODO: Run queue's consumers immediately
+		return nil
+	}
+
+	optionsObject := *b.defaultOptions
+	for i, n := 0, len(options); i < n; i++ {
+		if options[i] != nil {
+			options[i](&optionsObject)
 		}
 	}
 
-	return nil
-}
-
-// Flush flushes data of the entire system.
-func (b *Bokchoy) Flush() error {
-	return b.broker.Flush()
-}
-
-// Queue gets or creates a new queue.
-func (b *Bokchoy) Queue(name string) *Queue {
 	queue, ok := b.queues[name]
 	if !ok {
 		queue = &Queue{
 			name:           name,
 			broker:         b.broker,
-			serializer:     b.Serializer,
-			logger:         b.Logger.With(logging.String("component", "queue")),
-			tracer:         b.Tracer,
+			serializer:     b.serializer,
+			logger:         b.logger.With("bokchoy_queue_name", name),
 			wg:             b.wg,
 			defaultOptions: b.defaultOptions,
 			middlewares:    b.middlewares,
@@ -139,176 +191,154 @@ func (b *Bokchoy) Queue(name string) *Queue {
 	return queue
 }
 
-// Stop stops all queues and consumers.
-func (b *Bokchoy) Stop(ctx context.Context) {
-	fields := []logging.Field{
-		logging.String("queues", strings.Join(b.QueueNames(), ", ")),
-	}
-
-	b.Logger.Debug(ctx, "Stopping queues...", fields...)
-	for i := range b.queues {
-		b.queues[i].stop(ctx)
-	}
-	b.Logger.Debug(ctx, "Queues stopped", fields...)
-
-	if len(b.servers) == 0 {
-		return
-	}
-
-	fields = []logging.Field{
-		logging.String("servers", strings.Join(b.ServerNames(), ", ")),
-	}
-
-	b.Logger.Debug(ctx, "Stopping servers...", fields...)
-	for i := range b.servers {
-		b.servers[i].Stop(ctx)
-
-		b.wg.Done()
-	}
-	b.Logger.Debug(ctx, "Servers stopped", fields...)
-}
-
-// QueueNames returns the managed queue names.
-func (b *Bokchoy) QueueNames() []string {
-	names := make([]string, 0, len(b.queues))
-
-	for k := range b.queues {
-		names = append(names, k)
-	}
-
-	return names
-}
-
-// ServerNames returns the managed server names.
-func (b *Bokchoy) ServerNames() []string {
-	names := make([]string, 0, len(b.servers))
-
-	for i := range b.servers {
-		names = append(names, fmt.Sprintf("%s", b.servers[i]))
-	}
-
-	return names
-}
-
-func (b *Bokchoy) displayOutput(ctx context.Context, queueNames []string) {
-	buf := NewColorWriter(ColorBrightGreen)
-	buf.Write("%s\n", logo)
-	buf = buf.WithColor(ColorBrightBlue)
-
-	user, err := user.Current()
-	if err == nil {
-		hostname, err := os.Hostname()
-		if err == nil {
-			buf.Write("%s@%s %v\n", user.Username, hostname, Version)
-			buf.Write("- uid: %s\n", user.Uid)
-			buf.Write("- gid: %s\n\n", user.Gid)
-		}
-	}
-
-	buf.Write("[config]\n")
-	buf.Write(fmt.Sprintf("- concurrency: %d\n", b.defaultOptions.Concurrency))
-	buf.Write(fmt.Sprintf("- serializer: %s\n", b.Serializer))
-	buf.Write(fmt.Sprintf("- max retries: %d\n", b.defaultOptions.MaxRetries))
-	buf.Write(fmt.Sprintf("- retry intervals: %s\n", b.defaultOptions.RetryIntervalsDisplay()))
-	buf.Write(fmt.Sprintf("- ttl: %s\n", b.defaultOptions.TTL))
-	buf.Write(fmt.Sprintf("- countdown: %s\n", b.defaultOptions.Countdown))
-	buf.Write(fmt.Sprintf("- timeout: %s\n", b.defaultOptions.Timeout))
-	buf.Write(fmt.Sprintf("- tracer: %s\n", b.Tracer))
-	buf.Write(fmt.Sprintf("- broker: %s\n", b.broker))
-	buf.Write("\n[queues]\n")
-
-	for i := range queueNames {
-		buf.Write(fmt.Sprintf("- %s\n", queueNames[i]))
-	}
-
-	if len(b.servers) > 0 {
-		buf.Write("\n[servers]\n")
-
-		for i := range b.servers {
-			buf.Write(fmt.Sprintf("- %s", b.servers[i]))
-		}
-	}
-
-	log.Print(buf)
-}
-
 // Run runs the system and block the current goroutine.
-func (b *Bokchoy) Run(ctx context.Context, options ...Option) error {
-	opts := newOptions()
-	for i := range options {
-		options[i](opts)
+func (b *Bokchoy) Run(ctx context.Context) *ekaerr.Error {
+
+	b.sema.Lock()
+	// Can't defer b.sema.Unlock() cause of b.wg.Wait() at the end of function.
+
+	if b.isStarted {
+		b.sema.Unlock()
+		return ekaerr.RejectedOperation.
+			New("Bokchoy: Bokchoy already running").
+			Throw()
 	}
 
-	if len(opts.Servers) > 0 {
-		b.servers = opts.Servers
+	if err := b.broker.Ping(); err != nil {
+		b.sema.Unlock()
+		return err.
+			AddMessage("Bokchoy: Failed to run Bokchoy consumers").
+			AddFields("bokchoy_broker", b.broker.String()).
+			Throw()
 	}
 
-	err := b.broker.Ping()
-	if err != nil {
-		return err
+	queuesList := strings.Join(b.queueNames(), ", ")
+
+	if b.logger.IsValid() {
+		b.logger.Debug("Bokchoy: Starting queues and their consumers...",
+			"bokchoy_queues_list", queuesList)
 	}
 
-	queueNames := b.QueueNames()
-	if len(opts.Queues) > 0 {
-		queueNames = funk.FilterString(queueNames, func(queueName string) bool {
-			return funk.InStrings(opts.Queues, queueName)
-		})
+	for _, queue := range b.queues {
+		queue.start(ctx)
 	}
 
-	if len(queueNames) == 0 {
-		b.Logger.Debug(ctx, "No queue to run...")
+	b.isStarted = true
+	b.sema.Unlock()
 
-		return ErrNoQueueToRun
-	}
-
-	fields := []logging.Field{
-		logging.String("queues", strings.Join(queueNames, ", ")),
-	}
-
-	b.Logger.Debug(ctx, "Starting queues...", fields...)
-
-	for i := range b.queues {
-		if !funk.InStrings(queueNames, b.queues[i].Name()) {
-			continue
-		}
-
-		b.queues[i].start(ctx)
-	}
-
-	b.Logger.Debug(ctx, "Queues started", fields...)
-
-	fields = []logging.Field{
-		logging.String("servers", strings.Join(b.ServerNames(), ", ")),
-	}
-
-	if len(b.servers) > 0 {
-		b.Logger.Debug(ctx, "Starting servers...", fields...)
-
-		for i := range b.servers {
-			b.wg.Add(1)
-
-			go func(server Server) {
-				err := server.Start(ctx)
-				if err != nil {
-					b.Logger.Error(ctx, fmt.Sprintf("Receive error when starting %s", server), logging.Error(err))
-				}
-			}(b.servers[i])
-		}
-
-		b.Logger.Debug(ctx, "Servers started", fields...)
-	}
-
-	if !b.defaultOptions.DisableOutput {
-		b.displayOutput(ctx, queueNames)
+	if b.logger.IsValid() {
+		b.logger.Debug("Bokchoy: Queues and their consumers has been started.",
+			"bokchoy_queues_list", queuesList)
 	}
 
 	b.wg.Wait()
+	return nil
+}
+
+// Stop stops all queues and their consumers.
+//
+// Stopping of queues and consumers can not be failed (it's just goroutines).
+// So, there is no returned error object, cause it never fail.
+//
+// Does nothing if Bokchoy is not running.
+func (b *Bokchoy) Stop(ctx context.Context) {
+
+	b.sema.Lock()
+	defer b.sema.Unlock()
+
+	if !b.isStarted {
+		return
+	}
+
+	queuesList := strings.Join(b.queueNames(), ", ")
+
+	if b.logger.IsValid() {
+		b.logger.Debug("Bokchoy: Stopping queues and their consumers...",
+			"bokchoy_queues_list", queuesList)
+	}
+
+	for i := range b.queues {
+		b.queues[i].stop(ctx) // can not fail
+	}
+
+	if b.logger.IsValid() {
+		b.logger.Debug("Bokchoy: Queues and their consumers has been stopped.",
+			"bokchoy_queues_list", queuesList)
+	}
+}
+
+// Use append a new middleware to the system.
+// Does nothing if Bokchoy already running (Run() has called).
+func (b *Bokchoy) Use(sub ...func(Handler) Handler) *Bokchoy {
+
+	b.sema.Lock()
+	defer b.sema.Unlock()
+
+	if b.isStarted {
+		return b
+	}
+
+	for i, n := 0, len(sub); i < n; i++ {
+		if sub != nil {
+			b.middlewares = append(b.middlewares, sub...)
+		}
+	}
+
+	return b
+}
+
+// Empty empties initialized queues.
+// Returns an error of the first queue that can not be emptied.
+// Does nothing (but returns an error) if Bokchoy already running (Run() has called).
+func (b *Bokchoy) Empty(ctx context.Context) *ekaerr.Error {
+
+	b.sema.Lock()
+	defer b.sema.Unlock()
+
+	if b.isStarted {
+		return ekaerr.RejectedOperation.
+			New("Bokchoy: Bokchoy is running").
+			Throw()
+	}
+
+	for _, queue := range b.queues {
+		if err := queue.Empty(ctx); err.IsNotNil() {
+			return err.
+				AddMessage("Bokchoy: Failed to empty all queues").
+				Throw()
+		}
+	}
 
 	return nil
 }
 
+// ClearAll clears all queues in the broker and also removes all metadata.
+// Does nothing (but returns an error) if Bokchoy already running (Run() has called).
+func (b *Bokchoy) ClearAll() *ekaerr.Error {
+
+	b.sema.Lock()
+	defer b.sema.Unlock()
+
+	if b.isStarted {
+		return ekaerr.RejectedOperation.
+			New("Bokchoy: Bokchoy is running").
+			Throw()
+	}
+
+	return b.broker.ClearAll()
+}
+
 // Publish publishes a new payload to a queue.
-func (b *Bokchoy) Publish(ctx context.Context, queueName string, payload interface{}, options ...Option) (*Task, error) {
+func (b *Bokchoy) Publish(
+
+	ctx       context.Context,
+	queueName string,
+	payload   interface{},
+	options   ...Option,
+) (
+	*Task,
+	*ekaerr.Error,
+) {
 	return b.Queue(queueName).Publish(ctx, payload, options...)
 }
 
---
+++ bokchoy_private.go
@@ -0,0 +1,77 @@
+//
+// ORIGINAL PACKAGE
+// ( https://github.com/thoas/bokchoy )
+//
+//     Copyright © 2019. All rights reserved.
+//     Author: Florent Messa
+//     Contacts: florent.messa@gmail.com, https://github.com/thoas
+//     License: https://opensource.org/licenses/MIT
+//
+// HAS BEEN FORKED, HIGHLY MODIFIED AND NOW IS AVAILABLE AS
+// ( https://github.com/qioalice/bokchoy )
+//
+//     Copyright © 2020. All rights reserved.
+//     Author: Ilya Stroy.
+//     Contacts: qioalice@gmail.com, https://github.com/qioalice
+//     License: https://opensource.org/licenses/MIT
+//
+
+package bokchoy
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/qioalice/ekago/v2/ekasys"
+)
+
+
+// queueNames returns the managed queue names.
+func (b *Bokchoy) queueNames() []string {
+	names := make([]string, 0, len(b.queues))
+
+	for k := range b.queues {
+		names = append(names, k)
+	}
+
+	return names
+}
+
+func (b *Bokchoy) displayOutput(queueNames []string) {
+
+	buf := newColorWriter(colorBrightGreen)
+	buf.Write("%s\n", _LOGO)
+	buf = buf.WithColor(colorBrightBlue)
+
+	usr, err := user.Current()
+	if err == nil {
+		hostname, err := os.Hostname()
+		if err == nil {
+			buf.Write(" %s@%s %v\n", usr.Username, hostname, VERSION)
+			buf.Write(" - UID: %s\n", usr.Uid)
+			buf.Write(" - GID: %s\n\n", usr.Gid)
+		}
+	}
+
+	srn := b.serializer.Name()
+	srhr := b.serializer.IsHumanReadable()
+
+	buf.Write("	[config]\n")
+	buf.Write("	- Broker:          %s\n", b.broker.String())
+	buf.Write("	- Serializer:      %s (Human-readable: %t)\n", srn, srhr)
+	buf.Write("	- Concurrency:     %d\n", b.defaultOptions.Concurrency)
+	buf.Write("	- Max retries:     %d\n", b.defaultOptions.MaxRetries)
+	buf.Write("	- Retry intervals: %s\n", b.defaultOptions.retryIntervalsEncode())
+	buf.Write("	- TTL:             %s\n", b.defaultOptions.TTL)
+	buf.Write("	- Countdown:       %s\n", b.defaultOptions.Countdown)
+	buf.Write("	- Timeout:         %s\n", b.defaultOptions.Timeout)
+
+	buf.Write("\n	[queues]\n")
+
+	for i := range queueNames {
+		buf.Write(fmt.Sprintf("	- %s\n", queueNames[i]))
+	}
+
+	_, _ = ekasys.Stdout().Write(buf.Bytes())
+}
