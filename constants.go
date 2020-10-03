c2fa9144fe5b9b6b7bbaa7356ef281f2ddf214b4
Ilya Stroy <qioalice@gmail.com>
2020-10-03 16:01:42 +0300

    Finished refactor constants.go

--- constants.go
+++ constants.go
@@ -1,21 +1,43 @@
+//
+// ORIGINAL PACKAGE
+// ( https://github.com/thoas/bokchoy )
+//
+//     Copyright © 2019. All rights reserved.
+//     Author: Florent Messa
+//     Contacts: florent.messa@gmail.com, https://github.com/thoas
+//     License: https://opensource.org/licenses/MIT
+//
+// HAS BEEN FORKED, HIGHLY MODIFIED AND NOW IS AVAILABLE AS
+// ( https://github.com/qioalice/bokchoy )
+//
+//     Copyright © 2020. All rights reserved.
+//     Author: Ilya Stroy.
+//     Contacts: qioalice@gmail.com, https://github.com/qioalice
+//     License: https://opensource.org/licenses/MIT
+//
+
 package bokchoy
 
-import "time"
+import (
+	"time"
+)
 
+//goland:noinspection GoSnakeCaseUsage
 const (
-	logo = `
- _           _        _                 
-| |__   ___ | | _____| |__   ___  _   _ 
-| '_ \ / _ \| |/ / __| '_ \ / _ \| | | |
-| |_) | (_) |   < (__| | | | (_) | |_| |
-|_.__/ \___/|_|\_\___|_| |_|\___/ \__, |
-                                  |___/ `
-	defaultTimeout     = 180 * time.Second
-	defaultConcurrency = 1
-	defaultMaxRetries  = 3
-	defaultTTL         = 180 * time.Second
+	_LOGO = `
+	 _           _        _                 
+	| |__   ___ | | _____| |__   ___  _   _ 
+	| '_ \ / _ \| |/ / __| '_ \ / _ \| | | |
+	| |_) | (_) |   < (__| | | | (_) | |_| |
+	|_.__/ \___/|_|\_\___|_| |_|\___/ \__, |
+	                                  |___/ 
+`
+	_DEFAULT_TIMEOUT     = 180 * time.Second
+	_DEFAULT_CONCURRENCY = 1
+	_DEFAULT_MAX_RETRIES = 3
+	_DEFAULT_TTL         = 180 * time.Second
 
-	Version = "v0.2.0"
+	VERSION = "v0.2.0"
 )
 
 var defaultRetryIntervals = []time.Duration{
