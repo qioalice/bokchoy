072e628f88a81abf71247eaf0b92c190cc03a2f5
Ilya Stroy <qioalice@gmail.com>
2020-10-06 02:11:50 +0300

    Added dummy serializer (implements Serializer interface)

---
+++ serializer_dummy.go
@@ -0,0 +1,45 @@
+//
+// ORIGINAL PACKAGE
+// ( https://github.com/thoas/bokchoy )
+//
+//     Copyright © 2019. All rights reserved.
+//     Author: Florent Messa
+//     Contacts: florent.messa@gmail.com, https://github.com/thoas
+//     License: https://opensource.org/licenses/MIT
+//
+// HAS BEEN FORKED, HIGHLY MODIFIED AND NOW IS AVAILABLE AS
+// ( https://github.com/qioalice/bokchoy )
+//
+//     Copyright © 2020. All rights reserved.
+//     Author: Ilya Stroy.
+//     Contacts: qioalice@gmail.com, https://github.com/qioalice
+//     License: https://opensource.org/licenses/MIT
+//
+
+package bokchoy
+
+import (
+	"github.com/qioalice/ekago/v2/ekaerr"
+)
+
+type (
+	serializerDummy struct {}
+)
+
+func (_ *serializerDummy) Dumps(_ interface{}) ([]byte, *ekaerr.Error) {
+	return nil, nil
+}
+
+func (_ *serializerDummy) Loads(_ []byte, _ interface{}) *ekaerr.Error {
+	return nil
+}
+
+func (_ *serializerDummy) IsHumanReadable() bool {
+	return true
+}
+
+func (_ *serializerDummy) Name() string {
+	return "Internal dummy serializer. Does nothing."
+}
+
+var _ Serializer = (*serializerDummy)(nil)
