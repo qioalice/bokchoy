caa1a77c72b0bdeeca5a7b8672d2260db15d539a
Ilya Stroy <qioalice@gmail.com>
2020-10-05 16:56:27 +0300

    Finished refactor of serializer.go, serializer_json.go

--- serializer.go
+++ serializer.go
@@ -1,15 +1,31 @@
+//
+// ORIGINAL PACKAGE
+// ( https://github.com/thoas/bokchoy )
+//
+//     Copyright © 2019. All rights reserved.
+//     Author: Florent Messa
+//     Contacts: florent.messa@gmail.com, https://github.com/thoas
+//     License: https://opensource.org/licenses/MIT
+//
+// HAS BEEN FORKED, HIGHLY MODIFIED AND NOW IS AVAILABLE AS
+// ( https://github.com/qioalice/bokchoy )
+//
+//     Copyright © 2020. All rights reserved.
+//     Author: Ilya Stroy.
+//     Contacts: qioalice@gmail.com, https://github.com/qioalice
+//     License: https://opensource.org/licenses/MIT
+//
+
 package bokchoy
+
+import (
+	"github.com/qioalice/ekago/v2/ekaerr"
+)
 
 // Serializer defines an interface to implement a serializer.
 type Serializer interface {
-	Dumps(interface{}) ([]byte, error)
-	Loads([]byte, interface{}) error
-}
-
-// newSerializer initializes a new Serializer.
-func newSerializer(cfg SerializerConfig) Serializer {
-	switch cfg.Type {
-	default:
-		return JSONSerializer{}
-	}
+	Dumps(interface{}) ([]byte, *ekaerr.Error)
+	Loads([]byte, interface{}) *ekaerr.Error
+	IsHumanReadable() bool
+	Name() string
 }
--- serializer_json.go
+++ serializer_json.go
@@ -1,20 +1,84 @@
+//
+// ORIGINAL PACKAGE
+// ( https://github.com/thoas/bokchoy )
+//
+//     Copyright © 2019. All rights reserved.
+//     Author: Florent Messa
+//     Contacts: florent.messa@gmail.com, https://github.com/thoas
+//     License: https://opensource.org/licenses/MIT
+//
+// HAS BEEN FORKED, HIGHLY MODIFIED AND NOW IS AVAILABLE AS
+// ( https://github.com/qioalice/bokchoy )
+//
+//     Copyright © 2020. All rights reserved.
+//     Author: Ilya Stroy.
+//     Contacts: qioalice@gmail.com, https://github.com/qioalice
+//     License: https://opensource.org/licenses/MIT
+//
+
 package bokchoy
 
-import "encoding/json"
+import (
+	"encoding/hex"
 
-type JSONSerializer struct {
-}
+	"github.com/qioalice/ekago/v2/ekadanger"
+	"github.com/qioalice/ekago/v2/ekaerr"
 
-func (s JSONSerializer) Dumps(v interface{}) ([]byte, error) {
-	return json.Marshal(v)
-}
+	"github.com/davecgh/go-spew/spew" // deep dumper
+	"github.com/json-iterator/go" // fast than encoding/json
+	"github.com/modern-go/reflect2" // fast than reflect (w/ caching)
+)
 
-func (s JSONSerializer) Loads(data []byte, v interface{}) error {
-	return json.Unmarshal(data, v)
-}
-
-func (s JSONSerializer) String() string {
-	return "json"
-}
+type JSONSerializer struct {}
 
 var _ Serializer = (*JSONSerializer)(nil)
+
+func (_ JSONSerializer) Dumps(v interface{}) ([]byte, *ekaerr.Error) {
+	data, legacyErr := jsoniter.Marshal(v)
+	if legacyErr != nil {
+
+		vType := "<nil>"
+		if v != nil {
+			vType = reflect2.TypeOf(v).String()
+		}
+
+		return nil, ekaerr.InternalError.
+			Wrap(legacyErr, "Bokchoy.JsonSerializer: Failed to serialize").
+			AddFields(
+				"bokchoy_serializer_obj_data", spew.Sdump(v),
+				"bokchoy_serializer_obj_type", vType).
+			Throw()
+	}
+
+	return data, nil
+}
+
+func (_ JSONSerializer) Loads(data []byte, v interface{}) *ekaerr.Error {
+	legacyErr := jsoniter.Unmarshal(data, v)
+	if legacyErr != nil {
+
+		vAddr := ekadanger.TakeRealAddr(v)
+		vType := "<nil>"
+		if v != nil {
+			vType = reflect2.TypeOf(v).String()
+		}
+
+		return ekaerr.InternalError.
+			Wrap(legacyErr, "Bokchoy.JsonSerializer: Failed to deserialize").
+			AddFields(
+				"bokchoy_serializer_raw_data_as_hex", hex.EncodeToString(data),
+				"bokchoy_serializer_destination_type", vType,
+				"bokchoy_serializer_destination_value", vAddr).
+			Throw()
+	}
+
+	return nil
+}
+
+func (_ JSONSerializer) IsHumanReadable() bool {
+	return true
+}
+
+func (_ JSONSerializer) Name() string {
+	return "JSON, based on: https://github.com/json-iterator/go"
+}
