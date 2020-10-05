b23bf0cda333d755fca4bfcb7e72fc2642833089
Ilya Stroy <qioalice@gmail.com>
2020-10-06 02:08:30 +0300

    Broker's methods does not accepts context anymore

--- broker.go
+++ broker.go
@@ -19,7 +19,6 @@
 package bokchoy
 
 import (
-	"context"
 	"time"
 
 	"github.com/qioalice/ekago/v2/ekaerr"
@@ -28,20 +27,22 @@ import (
 
 // Broker is the common interface to define a Broker.
 type Broker interface {
+
 	// Initialize initializes the broker.
-	Initialize(context.Context) *ekaerr.Error
+	Initialize() *ekaerr.Error
 
 	// Ping pings the broker to ensure it's well connected.
 	Ping() *ekaerr.Error
 
-	// Get returns raw data stored in broker.
-	Get(string) (map[string]interface{}, *ekaerr.Error)
+	// Get returns serialized Task from the broker.
+	// You can call Task.Deserialize then to decode received data.
+	Get(taskKey string) ([]byte, *ekaerr.Error)
 
 	// Delete deletes raw data in broker based on key.
 	Delete(string, string) *ekaerr.Error
 
 	// List returns raw data stored in broker.
-	List(string) ([]map[string]interface{}, *ekaerr.Error)
+	List(string) ([][]byte, *ekaerr.Error)
 
 	// Empty empties a queue.
 	Empty(string) *ekaerr.Error
@@ -53,13 +54,13 @@ type Broker interface {
 	Count(string) (BrokerStats, *ekaerr.Error)
 
 	// Save synchronizes the stored item.
-	Set(string, map[string]interface{}, time.Duration) *ekaerr.Error
+	Set(string, []byte, time.Duration) *ekaerr.Error
 
 	// Publish publishes raw data.
-	Publish(string, string, map[string]interface{}, ekatime.Timestamp) *ekaerr.Error
+	Publish(queueName, taskID string, taskPayload []byte, taskETA ekatime.Timestamp) *ekaerr.Error
 
 	// Consume returns an array of raw data.
-	Consume(context.Context, string, ekatime.Timestamp) ([]map[string]interface{}, *ekaerr.Error)
+	Consume(string, ekatime.Timestamp) ([][]byte, *ekaerr.Error)
 
 	// String must present an info about Broker like:
 	// Name, DSN, settings, etc.
