0ecf3e7baf4d6b13c8c09437f0a4367d2250a01f
Ilya Stroy <qioalice@gmail.com>
2021-02-03 23:50:28 +0300

    Disabled saving completed tasks back to Broker.

--- consumer_private.go
+++ consumer_private.go
@@ -19,7 +19,6 @@
 package bokchoy
 
 import (
-	"fmt"
 	"sync/atomic"
 	"time"
 
@@ -27,6 +26,29 @@ import (
 )
 
 type (
+	// consumer is a some Queue's Task s executioner.
+	// One Queue will have as many consumers,
+	// as requested by WithConcurrency() option. Default is: _DEFAULT_CONCURRENCY.
+	//
+	// Main consumer's loop is started by requestStart() method,
+	// stops by requestStop()'s one,
+	// but it may stop itself if it's not master consumer.
+	//
+	// Master consumer it's a consumer with idx == 0.
+	// idx is sets by Queue at the consumer's creation.
+	// All other consumers are considered slaves.
+	//
+	// Master/Slave division of consumers is exist
+	// to decrease load to the either Broker, or any your service
+	// when an errors occurred one-by-one in a row, counting not only Broker errors,
+	// but Task's callbacks/handlers too.
+	//
+	// Don't worry, it's OK if sometimes your Task s are failed,
+	// or Broker returns an errors (while retrieving/saving tasks).
+	// But it's not, if there's ONLY errors, nothing more.
+	// Each task, each Broker's call, everything is failed.
+	//
+	// It guarantees, that NOT MORE THAN ONE loop may be run for one consumer.
 	consumer struct {
 
 		// WARNING!
@@ -52,13 +74,15 @@ type (
 //goland:noinspection GoSnakeCaseUsage
 const (
 	// TODO: Add an Option to manage this value instead of constant usage.
-	_CONSUMER_MAX_ERRORS_IN_A_ROW = 10
+	_CONSUMER_MAX_ERRORS_IN_A_ROW = 32
 
 	_CONSUMER_STATUS_ACTIVE  int32 = 1
 	_CONSUMER_STATUS_STOPPED int32 = 2
-	_CONSUMER_STATUS_FREEZED int32 = 3
+	_CONSUMER_STATUS_FROZEN  int32 = 3
 )
 
+// requestStart starts consumer's loop (if it's not started yet)
+// calling consumeLoop() in a new, separated goroutine.
 func (c *consumer) requestStart() {
 	c.queue.wg.Add(1)
 	if atomic.SwapInt32(&c.status, _CONSUMER_STATUS_ACTIVE) != _CONSUMER_STATUS_ACTIVE {
@@ -66,27 +90,34 @@ func (c *consumer) requestStart() {
 	}
 }
 
+// requestStop() asks consumer to stop its loop.
+// IT DOES NOT STOP LOOP IMMEDIATELY.
 func (c *consumer) requestStop() {
 	atomic.StoreInt32(&c.status, _CONSUMER_STATUS_STOPPED)
 }
 
+// consumeLoop() is consumer's loop. It just calls consumeLoopIter(),
+// checking whether loop can go to the next iteration after each one.
 func (c *consumer) consumeLoop() {
-
 	defer c.queue.wg.Done()
 
 	status := atomic.LoadInt32(&c.status)
 	for status == _CONSUMER_STATUS_ACTIVE ||
-		status == _CONSUMER_STATUS_FREEZED && c.idx == 0 {
+		status == _CONSUMER_STATUS_FROZEN && c.idx == 0 {
 
 		c.consumeLoopIter()
 		status = atomic.LoadInt32(&c.status)
 	}
 }
 
+// consumerLoopIter is consumer's loop iteration.
+//
+// It tries to retrieve next N tasks (depends of Broker.Consume())
+// and process all of them one-by-one using processTask() method.
 func (c *consumer) consumeLoopIter() {
 
 	tasks, err := c.queue.Consume()
-	c.handleError(err)
+	c.countErrorIfAny(err)
 
 	if len(tasks) == 0 {
 		return
@@ -101,78 +132,96 @@ func (c *consumer) consumeLoopIter() {
 	}
 
 	for i, n := 0, len(tasks); i < n; i++ {
-		err = c.handleTask(&tasks[i])
-		c.handleError(err)
+		err = c.processTask(&tasks[i])
+		c.countErrorIfAny(err)
 	}
 }
 
-func (c *consumer) handleTask(task *Task) *ekaerr.Error {
-	const s = "Bokchoy: Failed to handle task under consuming. "
+// processTask is the Task's processing entry point.
+// After this, Task may be only:
+//  - Returned back to the pool if it must be retried later,
+//  - Considering completed (w/ or w/o error/panic).
+//
+// Locks itself until Task is reached any of two states above, or till Task.Timeout.
+// If Task.Timeout is presented and reached,
+// no neither next callbacks is called nor handlers, but the last callback/handler
+// that might be under execution keeps locking separated goroutine,
+// until it's done.
+func (c *consumer) processTask(t *Task) *ekaerr.Error {
+	const s = "Bokchoy: Failed to process task under consuming. "
 
 	if c.queue.parent.logger.IsValid() {
 		c.queue.parent.logger.Debug("Bokchoy: Task processing is started.",
 			"bokchoy_queue_name", c.queue.name,
-			"bokchoy_task_id", task.id)
+			"bokchoy_task_id",    t.id)
 	}
 
-	if task.Timeout != 0 {
-
+	if t.Timeout != 0 {
 		var (
-			timeoutTimer = time.NewTimer(task.Timeout)
+			timeoutTimer = time.NewTimer(t.Timeout)
 			doneChan     = make(chan struct{})
 		)
 
-		// Invoke handlers asynchronously...
-		go c.fire(doneChan, task)
+		// Handlers and callbacks will be called in another goroutine,
+		// but all in the same.
+		go c.fire(doneChan, t)
 
-		// ... but lock the consumer until task is processed
-		// or until timeout comes.
 		select {
-		case _, _ = <- doneChan: // will be closed in c.invoker()
+		case _, _ = <- doneChan: // will be closed in c.fire()
 		case _, _ = <- timeoutTimer.C:
-			task.markAsTimedOut()
-
+			t.markAsTimedOut()
 			if c.queue.parent.logger.IsValid() {
 				c.queue.parent.logger.Warn(s + "Timed out.",
-					"bokchoy_task_id", task.id,
-					"bokchoy_task_timeout", task.Timeout,
-					"bokchoy_queue_name", c.queue.name)
+					"bokchoy_task_id",      t.id,
+					"bokchoy_task_timeout", t.Timeout,
+					"bokchoy_queue_name",   c.queue.name)
 			}
 		}
 
 		timeoutTimer.Stop() // GC timer
-
 	} else {
-		// Lock the consumer until task is processed.
-		c.fire(nil, task)
+		c.fire(nil, t)
 	}
 
-	// Maybe there is no need to save back to the broker, cause tash
-	// already published and under retrying?
+	if t.status == TASK_STATUS_RETRYING {
 
-	if task.status == TASK_STATUS_RETRYING {
-		return nil
-	}
+		// FIRST: calculating next ETA, THEN: decreasing MaxRetries.
+		// NOT VICE VERSA! PANIC OTHERWISE, INDEX OUT OF RANGE IN (*Task).nextETA().
+		t.ETA = t.nextETA()
+		t.MaxRetries--
 
-	if err := c.queue.save(task); err.IsNotNil() {
-		return err.
-			AddMessage(s + "Failed to save task after callbacks has been called " +
-				"or timeout comes.").
-			AddFields("bokchoy_task_status", task.status).
-			Throw()
+		if err := c.queue.save(t); err.IsNotNil() {
+			return err.
+				AddMessage(s + "Failed to return failed task to the pool " +
+					"for being retried later.").
+				Throw()
+		}
 	}
 
 	return nil
 }
 
-func (c *consumer) handleError(err *ekaerr.Error) {
+// countErrorIfAny implements 95% of starting/stopping/freezing master/slave
+// consumer s when error is occurred.
+//
+// It freezes all slave consumer s if Queue.errCounter is reached N
+// (Default is: _CONSUMER_MAX_ERRORS_IN_A_ROW).
+// It also unfreezes all slave consumer s if master consumer meet non-error call;
+// and counts all errors using Queue.errCounter.
+//
+// Writes logs, handles rare cases like stopping requested while freezing/unfreezing,
+// etc.
+func (c *consumer) countErrorIfAny(err *ekaerr.Error) {
 	const s = "Bokchoy: An error (%d) occurred while trying to consume tasks of queue. "
 
 	if err.IsNotNil() {
 
 		queueConsumeErrorCounter := atomic.LoadInt32(&c.queue.errCounter)
 		if queueConsumeErrorCounter >= _CONSUMER_MAX_ERRORS_IN_A_ROW {
-			atomic.StoreInt32(&c.status, _CONSUMER_STATUS_FREEZED)
+			// CAS because we don't want to change stopped -> frozen.
+			// It should never happen at all, but who knows the future, right?
+			atomic.CompareAndSwapInt32(&c.status,
+				_CONSUMER_STATUS_ACTIVE, _CONSUMER_STATUS_FROZEN)
 			return
 		}
 
@@ -187,7 +236,7 @@ func (c *consumer) handleError(err *ekaerr.Error) {
 					queueConsumeErrorCounter)
 			}
 
-			atomic.StoreInt32(&c.status, _CONSUMER_STATUS_FREEZED)
+			atomic.StoreInt32(&c.status, _CONSUMER_STATUS_FROZEN)
 			return
 		} else {
 
@@ -200,26 +249,32 @@ func (c *consumer) handleError(err *ekaerr.Error) {
 		}
 	} else {
 
-		prevStatus := atomic.SwapInt32(&c.queue.errCounter, 0)
-		unfreeze := prevStatus == _CONSUMER_STATUS_FREEZED && c.idx == 0
+		// Reset Queue's error counter.
+		atomic.StoreInt32(&c.queue.errCounter, 0)
+
+		// Whether it's master consumer and it must unfreeze others?
+		unfreeze :=
+			atomic.LoadInt32(&c.queue.errCounter) == _CONSUMER_STATUS_FROZEN &&
+			c.idx == 0
 
 		// Shutdown of consumers may requested at this moment.
-		// So, CAS is our protector. If it's not frozen atm, it's possible stopped.
+		// So, CAS is our protector.
+		// If it's not frozen atm, it's possible stopped.
 		// So, don't do something in that case.
 
 		if unfreeze {
 			masterConsumerActivated := atomic.CompareAndSwapInt32(&c.status,
-				_CONSUMER_STATUS_FREEZED, _CONSUMER_STATUS_ACTIVE)
+				_CONSUMER_STATUS_FROZEN, _CONSUMER_STATUS_ACTIVE)
 			unfreeze = unfreeze && masterConsumerActivated
 		}
 
-		// We need to unfreeze others consumers
+		// We need to unfreeze others consumers,
 		// if there are more than 1 consumers.
 
 		for i, n := 1, len(c.queue.consumers); unfreeze && i < n; i++ {
 
 			slaveConsumerActivated := atomic.CompareAndSwapInt32(&c.queue.consumers[i].status,
-				_CONSUMER_STATUS_FREEZED, _CONSUMER_STATUS_ACTIVE)
+				_CONSUMER_STATUS_FROZEN, _CONSUMER_STATUS_ACTIVE)
 			if slaveConsumerActivated {
 				c.queue.consumers[i].requestStart()
 			}
@@ -238,6 +293,9 @@ func (c *consumer) handleError(err *ekaerr.Error) {
 	}
 }
 
+// fire calls passed Task's handlers and callbacks, changing its status,
+// according with Task's state while execution callbacks.
+// Closes passed channel if it's not nil when fire is done.
 func (c *consumer) fire(done chan<- struct{}, task *Task) {
 
 	defer func(done chan<- struct{}) {
@@ -248,23 +306,21 @@ func (c *consumer) fire(done chan<- struct{}, task *Task) {
 
 	task.markAsProcessing()
 
-	// First of all call task status changed callbacks registered by
-	// Queue.OnStart() func.
+	// First of all call onStart callbacks.
 	c.fireEvents(task)
 
 	// c.fireEvents() may call many status changed callbacks and its status
 	// may be changed up to _TASK_MAX_STATUS_CHANGED_CALLBACKS_FIRING times.
+	//
 	// But if it's still TASK_STATUS_PROCESSING, we need to call handlers.
-
 	if task.status != TASK_STATUS_PROCESSING {
 		return
 	}
 
 	oldStatus := task.status
 	for i, n := 0, len(c.queue.handlers); i < n; i++ {
-		c._firSafeCall(c.queue.handlers[i], task)
-
-		if !c._firMayContinue(task) || oldStatus != task.status {
+		task.fireSafeCall(c.queue.handlers[i])
+		if !task.fireMayContinue() || oldStatus != task.status {
 			break
 		}
 	}
@@ -274,9 +330,7 @@ func (c *consumer) fire(done chan<- struct{}, task *Task) {
 	}
 
 	// This is the last time we need to call c.fireEvents().
-	// Assuming that now will be called callbacks registered only by
-	//  - Either Queue.OnSuccess() or Queue.OnFailure()
-	//  - And Queue.OnComplete().
+	// Expecting only onSuccess, onFailure callbacks will be called now.
 	c.fireEvents(task)
 }
 
@@ -290,37 +344,27 @@ func (c *consumer) fireEvents(task *Task) {
 	)
 
 	for ; i < _TASK_MAX_STATUS_CHANGED_CALLBACKS_FIRING; i++ {
-		switch task.status {
 
-		case TASK_STATUS_PROCESSING: handlers = c.queue.onStart
-		case TASK_STATUS_SUCCEEDED:  handlers = c.queue.onSuccess
-		case TASK_STATUS_FAILED:     handlers = c.queue.onFailure
-		case TASK_STATUS_CANCELLED:  handlers = c.queue.onFailure
+		switch ts := task.status; {
+		case ts == TASK_STATUS_PROCESSING: handlers = c.queue.onStart
+		case ts == TASK_STATUS_SUCCEEDED:  handlers = c.queue.onSuccess
+		case ts == TASK_STATUS_FAILED:     handlers = c.queue.onFailure
+		case ts == TASK_STATUS_CANCELLED:  handlers = c.queue.onFailure
+		case task.IsFinished():            handlers = c.queue.onComplete
 		}
 
 		oldStatus = task.status
 
+		// Starting execution one-by-one all handlers for the current Task's status,
+		// until they are over and while Task's status is not changed.
 		for i, n := 0, len(handlers); i < n; i++ {
-			c._firSafeCall(handlers[i], task)
-
-			if !c._firMayContinue(task) || oldStatus != task.status {
+			task.fireSafeCall(handlers[i])
+			if !task.fireMayContinue() || oldStatus != task.status {
 				break
 			}
 		}
 
-		if task.IsFinished() {
-			oldStatus = task.status
-
-			for i, n := 0, len(c.queue.onComplete); i < n; i++ {
-				c._firSafeCall(c.queue.onComplete[i], task)
-
-				if !c._firMayContinue(task) || oldStatus != task.status {
-					break
-				}
-			}
-		}
-
-		if !c._firMayContinue(task) ||
+		if !task.fireMayContinue() ||
 				oldStatus == task.status ||
 				task.status == TASK_STATUS_RETRYING {
 			break
@@ -328,79 +372,14 @@ func (c *consumer) fireEvents(task *Task) {
 	}
 
 	if i < _TASK_MAX_STATUS_CHANGED_CALLBACKS_FIRING ||
-		!c.queue.parent.logger.IsValid() {
+			!c.queue.parent.logger.IsValid() {
 		return
 	}
 
 	ekaerr.Interrupted.
 		New("Too many status changes.").
 		AddFields("bokchoy_last_status", task.status.String()).
-
 		LogAsWarnUsing(c.queue.parent.logger, s,
 			"bokchoy_task_id", task.id,
 			"bokchoy_queue_name", c.queue.name)
-}
-
-// Functions started with "_fir" is a part of fire() call.
-func (c *consumer) _firPanicProtector(task *Task) {
-	if panicked := recover(); panicked != nil {
-		task.Error = ekaerr.IllegalState.
-			New(fmt.Sprintf("Handler panicked: %+v", panicked)).
-			Throw()
-	}
-}
-
-// Functions started with "_fir" is a part of fire() call.
-func (c *consumer) _firSafeCall(handler HandlerFunc, task *Task) {
-	defer c._firPanicProtector(task)
-	if err := handler(task); err.IsNotNil() {
-		task.Error = err
-	}
-}
-
-// Functions started with "_fir" is a part of fire() call.
-func (c *consumer) _firMayContinue(task *Task) bool {
-	const s = "Bokchoy: Failed to republish task for being retried later. "
-
-	switch task.status {
-
-	default:
-		// Should never happen.
-		// Only TASK_STATUS_INVALID left, but it's cannot be assigned anywhere.
-		return false
-
-	case TASK_STATUS_FAILED:
-		return true
-
-	case TASK_STATUS_RETRYING, TASK_STATUS_CANCELLED, TASK_STATUS_TIMED_OUT:
-		return false
-
-	case TASK_STATUS_WAITING, TASK_STATUS_PROCESSING, TASK_STATUS_SUCCEEDED:
-		if task.Error.IsNil() && task.Panic == nil {
-			return true
-		}
-	}
-
-	// It's continue of the case when task.status is one of
-	// TASK_STATUS_WAITING, TASK_STATUS_PROCESSING, TASK_STATUS_SUCCEEDED,
-	// but there is an error occurred (task.Error.IsNotNil() == true) or
-	// a handler panicked and panic has been restored (task.Panic != nil).
-
-	if task.MaxRetries <= 0 {
-		task.markAsFailed()
-		return false
-	}
-
-	task.markAsRetrying()
-
-	if err := c.queue.PublishTask(task); err.IsNotNil() {
-		task.markAsFailed()
-
-		if c.queue.parent.logger.IsValid() {
-			err.LogAsErrorUsing(c.queue.parent.logger, s +
-				"Task will be marked as failed.")
-		}
-	}
-
-	return false
 }
